6ead152e032c9bdf04d67f1f562ee3a359b99a69
Oleg <irkstripman@yandex.ru>
2025-05-15 16:41:45 +0300

    finish prod

--- menu_app/models.py
+++ menu_app/models.py
@@ -3,10 +3,10 @@ from django.urls import reverse
 
 
 class MainMenu(models.Model):
-    title = models.CharField(max_length=50, verbose_name="Пункт меню", default='Название отсутствует')  # Наименование пункта меню
-    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')   # Ссылка на родительский пункт меню
-    route_name = models.CharField(max_length=200, verbose_name="Маршрут", blank=True, null=True)  # Имя маршрута для ссылки
-    url = models.URLField(verbose_name="Ссылка", max_length=200, blank=True, null=True)  # Ссылка на страницу
+    title = models.CharField(max_length=50, verbose_name="Пункт меню", default='Название отсутствует')
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children', verbose_name="Родительский пункт меню")
+    route_name = models.CharField(max_length=200, verbose_name="Имя маршрута для ссылки", blank=True, null=True)
+    url = models.URLField(verbose_name="Ссылка на страницу", max_length=200, blank=True, null=True)
 
 
     def __str__(self):
@@ -17,24 +17,22 @@ class MainMenu(models.Model):
         verbose_name = "Пункт меню"
 
     @property
-    def is_active(self, current_path=None):  # Проверка активности пункта меню
-        if not current_path:  # Если нет текущего пути, то вернуть False
+    def is_active(self, current_path=None):
+        if not current_path:
             return False
         try:
-            if self.route_name:  # Если есть имя маршрута
-                resolved_name = reverse(self.route_name)  # Получить имя маршрута
-                if resolved_name == current_path:  # Если имя маршрута совпадает с текущим путем
+            if self.route_name:
+                resolved_name = reverse(self.route_name)
+                if resolved_name == current_path:
                     return True
         except Exception as e:
             return False
 
-        # Затем проверим прямой URL
         if self.url:
             if self.url == current_path:
                 return True
 
 
-        #Рекурсивно проверим активных детей
         for child in self.children.all():
             if child.is_active(current_path):
                 return True
--- menu_app/templatetags/menu_tags.py
+++ menu_app/templatetags/menu_tags.py
@@ -3,33 +3,29 @@ from menu_app.models import MainMenu
 
 register = template.Library()
 
-def _draw_menu(menuitems, active_item_id=None, level=0):
-    """тэг для отрисовки меню"""
-    result = [] # список для сохранения строки меню
-    for i in menuitems:  # перебор элементов меню
-        children = i.children.all() if hasattr(i, 'children') else []  # получение всех дочерних элементов
-        active = i.id == active_item_id or any(child.id == active_item_id for child in children)  # проверка активности элемента меню
-        result.append(f'<li class="{"active" if active else ""}"><a href="{i.url}">{i.title}</a>')  # добавление строки меню
 
-        if len(children) > 0 and (level <= 1 or active):  # проверка на наличие дочерних элементов и уровень вложенности
-            result.append('<ul>' + "".join(_draw_menu(children, active_item_id, level + 1)) + '</ul>')  # рекурсивный вызов для дочерних элементов
-            result.append('</li>')  # закрытие тега списка
-    return ''.join(result)  # объединение всех строк в одну строку меню
+@register.simple_tag(takes_context=True)
+def draw_menu(context, parent_slug=None):
+    """ Кастомный тег для отображения меню """
+    def build_menu_items(menus):
+        """ Функция построения HTML-кода меню рекурсивно """
+        items = []
 
-@register.simple_tag(takes_context=True)  # регистрация тэга с параметром context
-def draw_menu(context, menu_name):  # функция для отрисовки меню
-    request = context['request']  # получение объекта запроса из контекста
-    current_path = request.path   # получение текущего пути запроса
-    root_items = MainMenu.objects.filter(parent__isnull=True).prefetch_related('children').all()  # получение корневых элементов меню с предварительной выборкой дочерних элементов
-    active_root = next((root for root in root_items if root.is_active(current_path)), None)  # получение активного корневого элемента меню
-    if active_root:  # если есть активный корневой элемент меню
-        first_level_children = active_root.children.all().prefetch_related('children')   # получение дочерних элементов первого уровня с предварительной выборкой дочерних элементов
-    else:
-        first_level_children = []  # если нет активного корневого элемента меню, то список дочерних элементов первого уровня будет пустым
-
-    html = '<ul>'   # начало строки меню
-    html += _draw_menu(root_items, active_root.id if active_root else None)  # вызов функции для отрисовки меню
-    html += '</ul>'  # конец строки меню
-    return html  # возврат строки меню
+        for menu in menus:
+            item_html = f"<li><a href='{menu.url}'>{menu.title}</a>"
 
 
+            children = MainMenu.objects.filter(parent=menu.id)
+            if children.exists():
+                item_html += '<ul>'
+                item_html += build_menu_items(children)
+                item_html += '</ul>'
+
+            item_html += "</li>"
+            items.append(item_html)
+
+        return ''.join(items)
+
+
+    root_menus = MainMenu.objects.filter(parent__isnull=True)
+    return f"<ul>{build_menu_items(root_menus)}</ul>"
